e8d2a356d762d9d4531abfd4c054a8acd1688e30
Minh Nghiem Quoc Anh 3 (FIS TDC HCM) <minhnqa3@fpt.com.vn>
2021-01-11 02:22:43 +0700

    Updates

---
+++ precache-manifest.36d5f43037e823f9cee3444d15cf654b.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec63787e8af1f726193fff9c7b203afb",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "d69820801254b4b79a81",
+    "url": "/Portfolio/static/css/main.84a674b3.chunk.css"
+  },
+  {
+    "revision": "7c54cc76dca1f74bdf78",
+    "url": "/Portfolio/static/js/2.b5dc11ec.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/Portfolio/static/js/2.b5dc11ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d69820801254b4b79a81",
+    "url": "/Portfolio/static/js/main.815c02a0.chunk.js"
+  },
+  {
+    "revision": "c66aa5eafc120060b1dc",
+    "url": "/Portfolio/static/js/runtime-main.efb1fe8d.js"
+  },
+  {
+    "revision": "0685327bad5eeb03ebda59c4e5b19ffc",
+    "url": "/Portfolio/static/media/COVID.0685327b.jpg"
+  },
+  {
+    "revision": "b76d63af45090d62aa7a8d767c3d0386",
+    "url": "/Portfolio/static/media/Dev_talks.b76d63af.jpg"
+  },
+  {
+    "revision": "421b2223526a6462fb15374ca06c9445",
+    "url": "/Portfolio/static/media/api.421b2223.png"
+  },
+  {
+    "revision": "8931471bc5121c5f5f684e080ef9fe8b",
+    "url": "/Portfolio/static/media/blinking.8931471b.JPG"
+  },
+  {
+    "revision": "d9ac3320fbd5bba51264f3e60f25cf17",
+    "url": "/Portfolio/static/media/bootstrap.d9ac3320.svg"
+  },
+  {
+    "revision": "6b5433ec6558cf134d5f6c33b724bac2",
+    "url": "/Portfolio/static/media/chatapp.6b5433ec.JPG"
+  },
+  {
+    "revision": "f69a82f60f211b15f91ad1897d867b69",
+    "url": "/Portfolio/static/media/color_classification.f69a82f6.jpg"
+  },
+  {
+    "revision": "7a7de336b31ec503c917212a9ab65986",
+    "url": "/Portfolio/static/media/css.7a7de336.svg"
+  },
+  {
+    "revision": "5e19a08444c8872ec77d89984e06b9dd",
+    "url": "/Portfolio/static/media/data.5e19a084.png"
+  },
+  {
+    "revision": "b5fcf8177543a0677eecdac1ec8cab41",
+    "url": "/Portfolio/static/media/facebook.b5fcf817.svg"
+  },
+  {
+    "revision": "e6fd74ef00d8a26f096a59167fb12f8f",
+    "url": "/Portfolio/static/media/git.e6fd74ef.svg"
+  },
+  {
+    "revision": "1f33e75d7b7df995b1609f8573ac8c9b",
+    "url": "/Portfolio/static/media/github.1f33e75d.svg"
+  },
+  {
+    "revision": "4ba98a823d931f922f94ff80977e61b0",
+    "url": "/Portfolio/static/media/hardware_store.4ba98a82.jpg"
+  },
+  {
+    "revision": "8d2f621318d9141b5f5ee5c40321c79f",
+    "url": "/Portfolio/static/media/html.8d2f6213.svg"
+  },
+  {
+    "revision": "22a776e9cc47a7f56ceb707fcede71c0",
+    "url": "/Portfolio/static/media/js.22a776e9.svg"
+  },
+  {
+    "revision": "a13311a36cde2cfa79bff078a24103bf",
+    "url": "/Portfolio/static/media/mightycoder.a13311a3.svg"
+  },
+  {
+    "revision": "82f6dd4ae54df664f0154e3465f7e2e7",
+    "url": "/Portfolio/static/media/new_year.82f6dd4a.jpg"
+  },
+  {
+    "revision": "f57e416135b7908c8591ac3c9a418788",
+    "url": "/Portfolio/static/media/pin.f57e4161.svg"
+  },
+  {
+    "revision": "45b48090de6a5fe37055f98339319dc4",
+    "url": "/Portfolio/static/media/portfolio.45b48090.JPG"
+  },
+  {
+    "revision": "2bbf54f4347eb668188629fb7384b3ce",
+    "url": "/Portfolio/static/media/react.2bbf54f4.svg"
+  },
+  {
+    "revision": "2185e423b1c8b70c671e1c927b95d4cd",
+    "url": "/Portfolio/static/media/tweetme.2185e423.PNG"
+  },
+  {
+    "revision": "ba526c82b455fdda70d3b6549fe09839",
+    "url": "/Portfolio/static/media/visual-studio-code.ba526c82.svg"
+  },
+  {
+    "revision": "a83b1acd3bde569cda149da3663772fc",
+    "url": "/Portfolio/static/media/web-design.a83b1acd.png"
+  }
+]);
